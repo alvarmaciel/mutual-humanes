43931002dc88b2e42edd8eba034b2ebc311371de
Alvar Maciel <alvarmaciel@gmail.com>
2023-06-09 10:01:44 -0300

    add first repo test and test

---
+++ humanes_api/humanes/infraestructure/repository.py
@@ -0,0 +1,26 @@
+import abc
+from humanes_api.humanes.domain.socies import AccountData
+
+
+class AbstractRepository(abc.ABC):
+    @abc.abstractmethod
+    def add(self, account_data: AccountData):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, reference) -> AccountData:
+        raise NotImplementedError
+
+
+class AccountDataRepository(AbstractRepository):
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, account_data):
+        self.session.add(account_data)
+
+    def get(self, reference):
+        return self.session.query(AccountData).filter_by(reference=reference).one()
+
+    def list(self):
+        return self.session.query(AccountData).all()
---
+++ humanes_api/tests/infraestructure/test_repo.py
@@ -0,0 +1,18 @@
+from sqlalchemy import text
+
+from humanes_api.humanes.domain.socies import Account, AccountData, AccountType
+from humanes_api.humanes.infraestructure import repository
+
+
+def test_repository_can_save_an_account_data(session):
+    # Setup
+    account_data = AccountData("a name", "a last name", "", "123456", "1245", "an address", "+5468", "an email")
+    # Exercise
+    repo = repository.AccountDataRepository(session)
+    repo.add(account_data)
+    session.commit()
+
+    # Verify
+    query = text("SELECT name, last_name, venture, dni FROM 'accounts_data'")
+    results = session.execute(query).fetchall()
+    assert results == [("a name", "a last name", "", "123456")]
